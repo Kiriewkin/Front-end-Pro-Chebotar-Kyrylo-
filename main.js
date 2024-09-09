2c8cd7a804d6b4ba4c14c94fc81b2bbdfe6fdc9b
Kiriewka <chebotar.kiryusha@gmail.com>
2024-09-09 05:31:27 +0300

    Add user input prompts and conditional responses for age, city, and hobby

---
+++ main.js
@@ -0,0 +1,27 @@
+let ageUser = prompt("Ваш год рождения?");
+let birthYear = +ageUser;
+let cityUser = prompt("В каком городе вы живете?");
+let hobbyUser = prompt("Ваш любимый вид спорта?");
+
+console.log(cityUser);
+console.log(hobbyUser);
+
+if (cityUser === "Киев") {
+    console.log("Ты живешь в столице Украины");
+} else if (cityUser === "Вашингтон"){
+    console.log("Ты живешь в столице США");
+} else if (cityUser === "Лондон"){
+    console.log("Ты живешь в столице Великобритании")
+} else {
+    console.log(`Ты живешь в городе ${cityUser}`)
+}
+
+if (ageUser === null || ageUser === "") {
+    birthYear = null;
+} else {
+    birthYear = +ageUser;
+}
+
+if (birthYear === null) {
+    console.log("Шкода, що Ви не захотіли ввести свій рік народження.");
+}
