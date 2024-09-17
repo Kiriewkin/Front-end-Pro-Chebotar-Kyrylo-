91ec94976c99968dc6c36fccffdb2df60b32f95d
Kiriewka <chebotar.kiryusha@gmail.com>
2024-09-17 12:54:59 +0300

    Write a function for selecting an array of symbols

---
+++ main.js
@@ -0,0 +1,18 @@
+let str = "hello world";
+let arr = ['l', 'd'];
+
+function eraseFromStr(str, arrayToDelete) {
+    let newStrArray = [];
+    let strArray = str.split('');
+
+    for (let i = 0; i < strArray.length; i++) {
+        let char = strArray[i];
+        if (!arrayToDelete.includes(char)) {
+            newStrArray.push(char);
+        }
+    }
+    return newStrArray.join('');
+}
+
+let result = eraseFromStr(str, arr);
+console.log(result);
