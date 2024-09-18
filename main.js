437449ecb1f04b5e04ebd8923c6948f29619fc75
Kiriewka <chebotar.kiryusha@gmail.com>
2024-09-18 18:10:53 +0300

    A loop function

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-function multiply(first) {
-    return function(second) {
-        return first * second;
+function askNumber() {
+    let attempts = 0;
+
+    while (attempts < 10) {
+        let userNumber = prompt("Введите число больше 100:");
+
+        if (userNumber === null) {
+            console.log("Ввод отменен");
+            return;
+        }
+
+        userNumber = Number(userNumber);
+
+        if (userNumber > 100) {
+            console.log(`Вы ввели ${userNumber}`);
+            return;
+        }
+
+        attempts++;
+
+        if (attempts === 10) {
+            console.log(`Максимальное количество попыток. Останнє введене число: ${userNumber}`);
+        }
     }
 }
 
-console.log(multiply(4)(2));
-console.log(multiply(4)(8));
+askNumber();
