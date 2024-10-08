064e289c3c28b19331de17dc5e012856ee010e6d
Kiriewka <chebotar.kiryusha@gmail.com>
2024-10-08 22:48:59 +0300

    TODO лист

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const parentDiv = document.querySelector(`#parentDiv`);
+const todoInput = document.querySelector(`#todoInput`);
+const todoBtn = document.querySelector(`#todoBtn`);
+const todoList = document.querySelector(`#todoList`);
 
-parentDiv.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        alert(`Вы нажали на: ${e.target.innerText}`);
+todoBtn.addEventListener(`click`, () => {
+    if (todoInput.value.trim() !== "") {
+        const newTodo = document.createElement(`li`);
+        const deleteTodo = document.createElement(`button`);
+
+        newTodo.textContent = todoInput.value;
+        newTodo.classList.add(`new-todo`);
+
+        deleteTodo.innerHTML = `Удалить`;
+        deleteTodo.classList.add(`delete-todo`);
+
+        todoList.appendChild(newTodo);
+        newTodo.appendChild(deleteTodo);
+
+        todoInput.value = ``;
+
+        deleteTodo.addEventListener(`click`, () => {
+            newTodo.remove();
+        });
+    } else {
+        alert("Введите текст задания");
     }
 });
