37be3222827ad07eb4e2b91c04653925d8cbaa25
Kiriewka <chebotar.kiryusha@gmail.com>
2024-10-25 23:30:35 +0300

    Bank account

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-class Coach {
+class BankAccount {
 
-    constructor(name, specialization, rate) {
-        this.name = name;
-        this.specialization = specialization;
-        this.rate = rate;
+    constructor(balance) {
+        this.balance = balance;
     }
 
-    displayInfo() {
-        console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rate}`);
+    deposit(sum) {
+        if(sum < 0) {
+            console.log(`Сумма для пополнения должна быть больше нуля`)
+        } else {
+            this.balance += sum;
+            console.log(`Успешно пополнено ${sum}. Новый баланс: ${this.balance}`)
+            return this.balance
+        }
+    }
+
+    withdraw(sum) {
+        if (sum > this.balance) {
+            console.log(`Не хватает средств для операции`);
+            return false;
+        } else if (sum <= 0) {
+            console.log(`Сумма для снятия должна быть больше нуля`);
+            return false;
+        } else {
+            this.balance -= sum;
+            console.log(`Успешно снято ${sum}. Новый баланс: ${this.balance}`);
+            return this.balance
+        }
+    }
+    
+    getBalance() {
+        return this.balance;
     }
 }
 
-const coach1 = new Coach('John Doe', 'Fitness', 4.7);
-const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+const account1 = new BankAccount(1000);
 
-coach1.displayInfo();
-coach2.displayInfo();
+console.log(account1.getBalance());
+
+account1.deposit(500);
+console.log(account1.getBalance());
+
+account1.withdraw(200);
+console.log(account1.getBalance());
